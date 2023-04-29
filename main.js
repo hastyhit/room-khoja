9c61636a0712e004f938da3bcf7d835ce787feb9
hastyhit <saantoshlama@gmail.com>
2023-04-29 18:39:36 +0545

    minor update

--- main.js
+++ main.js
@@ -24,5 +24,25 @@ function toSignUpPage() {
 const d = new Date();
 document.getElementById("time").innerHTML = d;
 
+// pop up button
+
+let popUp = document.getElementById("popUp")
+
+function openPopup() {
+    popUp.classList.add("popUp-open");
+}
+
+function closePopup() {
+    popUp.classList.remove("popUp-open");
+}
+
+
+// loop through all apply button
+const apply = document.querySelectorAll(".r-button");
+let applyLength = apply.length;
+
+for (let i = 0; i < applyLength; i++) {
+    apply[i].addEventListener("click", openPopup);
+}
 
 
